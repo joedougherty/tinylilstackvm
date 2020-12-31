f3f0bf18b0f3cc80d82470eb596110d7d457cab5
Joe Dougherty <joseph.dougherty@gmail.com>
2020-12-31 14:53:37 -0500

    rearrange parser for better testability

--- tinylilparser.py
+++ tinylilparser.py
@@ -1,12 +1,23 @@
 def parse_program(fname):
+    lines = raw_lines(fname)
+    instructions = process_lines(lines)
+    symbol_table = generate_symbol_table(instructions)
+    linked_instructions = link(instructions, symbol_table)
+    return linked_instructions
+
+
+def raw_lines(fname):
     with open(fname) as f:
         file_contents = f.readlines()
-    
+    return file_contents
+
+
+def process_lines(file_contents):
     instructions = []
     for line in file_contents:
         instructions += parse_line(line)
 
-    return link(instructions)
+    return instructions
 
 
 def parse_line(line):
@@ -29,9 +40,7 @@ def parse_line(line):
     return agg
 
 
-def link(instructions):
-    symbol_table = generate_symbol_table(instructions)
-    
+def link(instructions, symbol_table):
     resolved_instructions = []
     for inst in instructions:
         if inst in symbol_table.keys():
