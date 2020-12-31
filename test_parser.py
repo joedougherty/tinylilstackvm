9e8562a5181e13d7960049555a0744435d82fe38
Joe Dougherty <joseph.dougherty@gmail.com>
2020-12-31 13:57:20 -0500

    new tests

--- test_parser.py
+++ test_parser.py
@@ -3,24 +3,22 @@ from tinylilparser import parse_program
 
 def test_maxfun_bc():
     instructions = [
-        "PUSH", 6,        # Push the first argument
-        "PUSH", 4,        # Push the second argument
-        "CALL", 7,        # Call "max"
-        "HALT",
-
-        # Here is address 7, the start of "max" function
-        "STORE", 1,       # Store b in local variable 1; the stack now contains [a]
-        "STORE", 0,       # Store a in local variable 0; the stack is now empty
-        "LOAD", 0,        # The stack now contains [a]
-        "LOAD", 1,        # The stack now contains [a, b]
-        "ISGE",           # The stack now contains [a > b]
-        "JIF", 21,        # If the top of the stack is true (a > b), jump to the "if" path
-        "LOAD", 1,        # "else" path: load b on the stack
-        "RET",
-
-        # Here is address 23
-        "LOAD", 0,        # "if" path: load a on the stack
-        "RET"
+        'PUSH', 6,
+        'PUSH', 4,
+        'CALL', 7,
+        'HALT',
+        'MAX:',
+        'STORE', 1,
+        'STORE', 0,
+        'LOAD',  0,
+        'LOAD',  1,
+        'ISGE',
+        'JIF',   22,
+        'LOAD',  1,
+        'RET',
+        'ENDMAX:',
+        'LOAD',  0,
+        'RET'
     ]
 
     assert instructions == parse_program('code/maxfun.bc')
